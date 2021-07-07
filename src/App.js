d38cd66bf3e0cc84598fc2ea4456482d785cb4f3
elik <elik@example.com>
2021-07-07 20:14:15 +0300

    start-project

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="modal">
+      <form action="post">
+         <h1 className="modal__title">Create a new account</h1>
+         <input className="modal__name-input" type="text" placeholder="Enter your name"/>
+         <input className="modal__mail-input" type="text" placeholder="Email"/>
+         <input className="modal__pass-input" type="password" placeholder="Password"/>
+         <div className="modal-select-counry">Latvia</div>
+         <div className="modal__sex-wrap">
+            <p><input type="radio" id="M"  value="Male" name="sex"/>Male</p>
+            <p><input type="radio" id="F" value="Female" name="sex"/>Female</p>
+         </div>
+         <div className="modal__accept">
+            <input type="checkbox" id="accept"/>
+            <label for="accept"> Accept <a href="/">terms</a> and <a href="/">conditions</a></label>
+         </div>
+         <button className="modal__button" type="submit">Sign up</button>
+      </form>
+   </div>
   );
 }
 
